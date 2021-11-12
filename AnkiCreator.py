9af5624f7a03b2f2056ead6de4213991662b32d5
Philipp <philipp@smponias.de>
2021-11-12 18:09:48 +0100

    Fixed bug mentioned in #3. This way you don't have to worry about unique file names.

--- AnkiCreator.py
+++ AnkiCreator.py
@@ -1,39 +1,40 @@
-import genanki
 import os
 import random
 
-def createAPKG(name):
-  MODELID = random.randrange(1 << 30, 1 << 31)
-  my_model = genanki.Model(
-  MODELID,
-  'Example',
-  fields=[
-    {'name': 'Question'},
-    {'name': 'Answer'},
-  ],
-  templates=[
-    {
-      'name': 'Card 1',
-      'qfmt': '{{Question}}',
-      'afmt': '{{FrontSide}}<hr id="answer">{{Answer}}',
-    },
-  ])
-  deckId = random.randrange(1 << 30, 1 << 31)
-  my_deck = genanki.Deck(
-    deckId,
-    name)
-  DIRECTORY = "Temp"
-  count=0
-  filelist = sorted(os.listdir(DIRECTORY))
-  filelist.remove('input.pdf')
-  #print(filelist)
-  while count<len(filelist):
-    field1 = '<img src="%s"\>' % (filelist[count])
-    field2 = '<img src="%s"\>' % (filelist[count+1])
-    my_note = genanki.Note(model=my_model,fields=[field1,field2])
-    my_deck.add_note(my_note)
-    count=count+2
-  my_package = genanki.Package(my_deck)
-  for x in filelist:
-    my_package.media_files.append('Temp'+os.sep+x)
-  my_package.write_to_file('%s.apkg' % name)
+import genanki
+
+
+def create_apkg(name):
+    MODELID = random.randrange(1 << 30, 1 << 31)
+    my_model = genanki.Model(
+        MODELID,
+        "Example",
+        fields=[
+            {"name": "Question"},
+            {"name": "Answer"},
+        ],
+        templates=[
+            {
+                "name": "Card 1",
+                "qfmt": "{{Question}}",
+                "afmt": '{{FrontSide}}<hr id="answer">{{Answer}}',
+            },
+        ],
+    )
+    deckId = random.randrange(1 << 30, 1 << 31)
+    my_deck = genanki.Deck(deckId, name)
+    DIRECTORY = "Temp"
+    count = 0
+    filelist = sorted(os.listdir(DIRECTORY))
+    filelist.remove("input.pdf")
+    # print(filelist)
+    while count < len(filelist):
+        field1 = r'<img src="%s"\>' % (filelist[count])
+        field2 = r'<img src="%s"\>' % (filelist[count + 1])
+        my_note = genanki.Note(model=my_model, fields=[field1, field2])
+        my_deck.add_note(my_note)
+        count = count + 2
+    my_package = genanki.Package(my_deck)
+    for x in filelist:
+        my_package.media_files.append("Temp" + os.sep + x)
+    my_package.write_to_file("%s.apkg" % name)
--- paper2anki.py
+++ paper2anki.py
@@ -1,62 +1,78 @@
-from AnkiCreator import createAPKG
-import sys,shutil
-import os,glob
+import glob
+import os
 import re
-from pdfrw import PdfReader, PdfWriter, PageMerge
+import shutil
+import sys
+import time
+
 import fitz
 from PIL import Image
+from pdfrw import PdfReader, PdfWriter, PageMerge
 
+from AnkiCreator import create_apkg
 
 TEMP = "Temp"
 if not os.path.exists(TEMP):
     os.makedirs(TEMP)
 
-''' 
+"""
 Split a page into two (top and bottom)
 Source: https://stackoverflow.com/a/31640152/5861086
 credits to Patrick Maupin
 I have modified it, so the pages do not contain empty block
-'''
+"""
+
+
 def splitpage(src):
     for y_pos in (0, 0.5):
         # Create a blank, unsized destination page.
         page = PageMerge()
         page.add(src, viewrect=(0, y_pos, 1, 0.5))
-        yield page.render()        
+        yield page.render()
+
+
 def createPDF(pdf):
     writer = PdfWriter()
     for page in PdfReader(pdf).pages:
         writer.addpages(splitpage(page))
-    writer.write(TEMP+os.sep+"input.pdf")
+    writer.write(TEMP + os.sep + "input.pdf")
 
-def createImages():
+
+def create_images():
+    current_time_in_millis = str(int(time.time_ns()))
     zoom_x = 2.0  # horizontal zoom
     zomm_y = 2.0  # vertical zoom
     mat = fitz.Matrix(zoom_x, zomm_y)  # zoom factor 2 in each dimension
-    doc = fitz.open(TEMP+os.sep+"input.pdf")
+    doc = fitz.open(TEMP + os.sep + "input.pdf")
     for x in doc:
-        pix = x.get_pixmap(alpha=False,matrix=mat)
+        pix = x.get_pixmap(alpha=False, matrix=mat)
         img = Image.frombytes("RGB", [pix.width, pix.height], pix.samples)
-        img.save("%s%s.jpg" %(TEMP+os.sep,format(x.number, '04d')))
-        #pix.writePNG("%i.png" % x.number)
-        
-def renameFiles():
+        # To make every image name in each file unique we will use the unix timestamp of the current day as a name extra
+        img.save(
+            "%s%s.jpg"
+            % (TEMP + os.sep, format(x.number, "04d") + "-" + current_time_in_millis)
+        )
+
+
+def rename_files():
     regex = r"[0-9]+\.jpg"
-    filelist = glob.glob('Temp'+os.sep+'*.jpg')
+    filelist = glob.glob("Temp" + os.sep + "*.jpg")
     for filename in filelist:
-        x = re.search(regex,filename)
-        os.rename(str(filename),"Temp"+os.sep++x.group())    
-def removeTemp():
+        x = re.search(regex, filename)
+        os.rename(str(filename), "Temp" + os.sep + x.group())
+
+
+def remove_temp():
     try:
         shutil.rmtree(TEMP)
     except OSError as e:
-        print("Error: %s : %s" % (TEMP, e.strerror))
+        print(f"Error: {TEMP} : {e.strerror}")
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     pdf = sys.argv[1]
     name = sys.argv[2]
     createPDF(pdf)
-    createImages()
-    #renameFiles()
-    createAPKG(name)
-    removeTemp()
+    create_images()
+    create_apkg(name)
+    remove_temp()
